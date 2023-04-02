999b035cbf59ab936dccbc21acac66d18b973093
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-02 15:13:33 +0600

    install react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
   return (
     <div className="App">
 
-      bootsTrap btn
+      <div>
+        {/* bootsTrap btn-----------------
       <button type="button" className="btn btn-primary">Primary</button>
-      
+
       <button type="button" className="btn btn-secondary">Secondary</button>
       <button type="button" className="btn btn-success">Success</button>
       <button type="button" className="btn btn-danger">Danger</button>
@@ -21,34 +22,37 @@ function App() {
       <button type="button" className="btn btn-dark">Dark</button>
       <button type="button" className="btn btn-link">Link</button>
 
+      bootsTrap card group ------ 
       <div>
-      <div className="card-group">
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
+        <div className="card-group">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div> */}
       </div>
+
 
     </div>
   )
