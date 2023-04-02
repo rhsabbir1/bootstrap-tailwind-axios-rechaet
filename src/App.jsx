e0bedc310a6c0567a01716917cc6fa07111e10cd
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-02 15:19:55 +0600

    use import css link

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,47 @@ function App() {
     <div className="App">
 
       <div>
-        {/* bootsTrap btn-----------------
-      <button type="button" className="btn btn-primary">Primary</button>
+        <p>bootsTrap btn-----------------</p>
+        <button type="button" className="btn btn-primary">Primary</button>
 
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
-      <button type="button" className="btn btn-link">Link</button>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" className="btn btn-danger">Danger</button>
+        <button type="button" className="btn btn-warning">Warning</button>
+        <button type="button" className="btn btn-info">Info</button>
+        <button type="button" className="btn btn-light">Light</button>
+        <button type="button" className="btn btn-dark">Dark</button>
+        <button type="button" className="btn btn-link">Link</button>
 
-      bootsTrap card group ------ 
-      <div>
-        <div className="card-group">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <p>bootsTrap card group ------</p>
+        <div>
+          <div className="card-group">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
-          </div>
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
-          </div>
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
-      </div> */}
       </div>
 
 
